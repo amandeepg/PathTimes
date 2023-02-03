197be8857ccb43f1cd088b03d4f4742f53258b5f
Amandeep Grewal <me@amandeep.ca>
2023-02-03 01:37:24 -0500

    Customize splash screen to use icon without background

--- app/src/main/java/ca/amandeep/path/MainActivity.kt
+++ app/src/main/java/ca/amandeep/path/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -23,6 +24,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         setContent {
             val navController = rememberNavController()
