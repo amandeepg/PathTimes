9ca4fbb7b088acd99eb907d54937a441c0451fed
Amandeep Grewal <me@amandeep.ca>
2023-06-01 00:22:10 -0400

    Use source sets instead of BuildConfig

--- app/src/debug/java/ca/amandeep/path/PathApplication.kt
+++ app/src/debug/java/ca/amandeep/path/PathApplication.kt
@@ -7,8 +7,6 @@ import timber.log.Timber.DebugTree
 class PathApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(DebugTree())
-        }
+        Timber.plant(DebugTree())
     }
 }
---
+++ app/src/release/java/ca/amandeep/path/PathApplication.kt
@@ -0,0 +1,5 @@
+package ca.amandeep.path
+
+import android.app.Application
+
+class PathApplication : Application()
