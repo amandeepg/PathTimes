51b5187b0404d975f334705787b3ba1c6f0a4e01
Amandeep Grewal <me@amandeep.ca>
2023-08-01 23:18:00 -0400

    Fix Alerts API

--- app/src/main/kotlin/data/PathAlertsApiService.kt
+++ app/src/main/kotlin/data/PathAlertsApiService.kt
@@ -8,12 +8,18 @@ import retrofit2.http.Header
 
 interface PathAlertsApiService {
     @GET("fetch?contentKey=PathAlert")
-    suspend fun getAlerts(@Header("apikey") apiKey: String = API_KEY): AlertContainer
+    suspend fun getAlerts(
+        @Header("apikey") apiKey: String = API_KEY,
+        @Header("appname") appName: String = APP_NAME,
+        @Header("appversion") appVersion: String = APP_VERSION,
+    ): AlertContainer
 
     companion object {
         private const val API_PATH = "https://path-mppprod-app.azurewebsites.net/api/v1/AppContent/"
 
         private const val API_KEY = "206A2FF9-0EA6-4C88-B030-5727054CB9DE"
+        private const val APP_NAME = "RidePATH"
+        private const val APP_VERSION = "5.2.0"
 
         val INSTANCE: PathAlertsApiService by lazy {
             Retrofit.Builder()
