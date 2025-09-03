a804ee44a074dbb102daba88b3d05013f78a171f
Amandeep Grewal <me@amandeep.ca>
2025-09-03 02:54:42 -0400

    Fix lint

--- android/data/src/main/java/ca/amandeep/path/data/AlertCache.kt
+++ android/data/src/main/java/ca/amandeep/path/data/AlertCache.kt
@@ -6,7 +6,6 @@ import com.github.ajalt.timberkt.d
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import com.squareup.moshi.adapter
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import okio.buffer
@@ -33,6 +32,7 @@ class AlertCache(
             .add(Date::class.java, Rfc3339DateJsonAdapter())
             .build()
     }
+
     @OptIn(ExperimentalStdlibApi::class)
     private val cacheAdapter: JsonAdapter<CachedAlertSummary> by lazy {
         moshi.adapter()
